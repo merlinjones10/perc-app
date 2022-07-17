eed2884e4548ebd82692f733e85aeec175488fb5
merlinjones10 <78985319+merlinjones10@users.noreply.github.com>
2022-07-17 21:58:19 +0100

    typeorm init

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,10 @@
 import express, { Request, Response } from 'express';
 import "reflect-metadata"
 import { PostController } from '../controllers/post.controller';
+import { AppDataSource } from "./data-source"
+import { User } from "./entity/User"
+
+
 
 class Server {
     private app: express.Application;
@@ -18,13 +22,24 @@ class Server {
     }
 
     public async routes(){
-        await createConnection({
-            type: 'postgres',
-            host: 'localhost',
-            port: 5432,
-            username: 'merlinjones',
-        })
 
+    AppDataSource.initialize().then(async () => {
+
+    console.log("Inserting a new user into the database...")
+    const user = new User()
+    user.firstName = "Timber"
+    user.lastName = "Saw"
+    user.age = 25
+    await AppDataSource.manager.save(user)
+    console.log("Saved a new user with id: " + user.id)
+
+    console.log("Loading users from the database...")
+    const users = await AppDataSource.manager.find(User)
+    console.log("Loaded users: ", users)
+
+    console.log("Here you can setup and run express / fastify / any other framework.")
+
+}).catch(error => console.log(error))
 
         this.app.use('/api/posts', this.postController.router);
         this.app.get('/', (req: Request, res: Response) => {
